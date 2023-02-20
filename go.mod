151390bdd71b6201a8350662c97ad99aeed0b5a6
jose <jomoespe@gmail.com>
2023-02-20 15:06:09 +0100

    Change module name to github.com/the-walking-dev/timestamp

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module github.com/digitorus/timestamp
+module github.com/the-walking-dev/timestamp
 
-go 1.16
+go 1.20
 
 require github.com/digitorus/pkcs7 v0.0.0-20221019075359-21b8b40e6bb4
--- timestamp_example_test.go
+++ timestamp_example_test.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/digitorus/timestamp"
+	"github.com/the-walking-dev/timestamp"
 )
 
 // ExampleCreateRequest_ParseResponse demonstrates the creation of a time-stamp request, sending
